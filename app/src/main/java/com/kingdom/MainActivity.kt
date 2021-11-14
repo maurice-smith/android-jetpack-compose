b3960435549aeddf9db606280641cc320725af3a
Maurice Smith <mo.maurice@gmail.com>
2021-11-13 21:10:31 -0500

    adding compose deps

--- app/src/main/java/com/kingdom/MainActivity.kt
+++ app/src/main/java/com/kingdom/MainActivity.kt
@@ -2,24 +2,16 @@ package com.kingdom
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.kingdom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewBinding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewBinding.lifecycleOwner = this
+        setContentView(R.layout.activity_main)
 
-        val toolbar = viewBinding.toolbar
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        toolbar.setupWithNavController(navHostFragment.navController)
+        //val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        //toolbar.setupWithNavController(navHostFragment.navController)
 
-        setSupportActionBar(toolbar)
+        //setSupportActionBar(toolbar)
     }
 }
--- app/src/main/java/com/kingdom/view/HelloWorldFragment.kt
+++ app/src/main/java/com/kingdom/view/HelloWorldFragment.kt
@@ -4,23 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.kingdom.R
-import com.kingdom.databinding.FragmentHelloWorldBinding
 
 class HelloWorldFragment : Fragment() {
-
-    private lateinit var viewBinding: FragmentHelloWorldBinding
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_hello_world, container, false)
-        viewBinding.lifecycleOwner = viewLifecycleOwner
-        return viewBinding.root
+        return inflater.inflate(R.layout.fragment_hello_world, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
